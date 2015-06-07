c6e3d9c5cacf5b7615240e70374104229c5fe094
Mario Ban <mario.ban@bluewin.ch>
2015-06-07 13:15:53 +0200

    Clean-up

--- examples/TestSimpleQtLogger/main.cpp
+++ examples/TestSimpleQtLogger/main.cpp
@@ -19,9 +19,12 @@ int main(int argc, char *argv[])
 {
   QCoreApplication a(argc, argv);
 
+  // initialize SimpleQtLogger
   simpleQtLogger_.setLogFileName("testSimpleQtLogger.log", 10000, 10); // TODO test
+  SQT_LOG_ENABLE_DEBUG = false;
   SQT_LOG_ENABLE_FUNCTION = true;
 
+  // start and initialize the main task
   Task *task = new Task(&a);
   QObject::connect(task, SIGNAL(finished()), &a, SLOT(quit()));
   QTimer::singleShot(0, task, SLOT(init()));
