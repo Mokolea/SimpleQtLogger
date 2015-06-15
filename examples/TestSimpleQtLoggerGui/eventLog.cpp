8d7d0a48c152a018426c083025df38cfe18f8bfb
Mario Ban <mario.ban@bluewin.ch>
2015-06-15 10:23:17 +0200

    Print event count

--- examples/TestSimpleQtLoggerGui/eventLog.cpp
+++ examples/TestSimpleQtLoggerGui/eventLog.cpp
@@ -27,6 +27,8 @@ EventLog::~EventLog()
 bool EventLog::eventFilter(QObject *obj, QEvent *event)
 {
   // L_FUNC("");
+  static unsigned int eventCount = 0;
+  eventCount++;
 
   if(obj && event) {
     const QMetaObject* metaObject = obj->metaObject();
@@ -36,7 +38,7 @@ bool EventLog::eventFilter(QObject *obj, QEvent *event)
       int eventTypeEnumIndex = QEvent::staticMetaObject.indexOfEnumerator("Type");
       QString eventTypeName = QEvent::staticMetaObject.enumerator(eventTypeEnumIndex).valueToKey(eventType);
 
-      QString text = QString("%1: QEvent type=%2 '%3'").arg(metaObject->className()).arg(eventType).arg(eventTypeName);
+      QString text = QString("%1: %2 event-type = %3 '%4'").arg(eventCount).arg(metaObject->className()).arg(eventType).arg(eventTypeName);
       emit eventInfo(text);
     }
   }
